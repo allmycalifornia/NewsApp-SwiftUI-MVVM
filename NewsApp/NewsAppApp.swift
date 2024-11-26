fcef92649833c6140ff970e5f78c87faa0861aae
Boris Kravchenko <allmycalifornia@gmail.com>
2024-11-26 13:45:49 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Борис Кравченко on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
